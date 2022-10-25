d6344e509f5e02076b0d0a51e489653c8ca8672b
gopikrishna456 <111789923+gopikrishna456@users.noreply.github.com>
2022-10-24 22:11:36 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Jonnalagadda Gopikrishna' });
+  res.render('mydata', { title: 'Jonnalagadda Gopikrishna' });
 });
 
 module.exports = router;
